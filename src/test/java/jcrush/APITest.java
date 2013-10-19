9cb7105501ab0d62e84770a843dac8b9beaa53da
Eddie <eddie@gamezgalaxy.com>
2013-10-19 01:05:38 -0400

    Started JUnit testing

---
+++ src/test/java/jcrush/APITest.java
@@ -0,0 +1,13 @@
+package jcrush;
+
+public class APITest {
+    @org.junit.Before
+    public void setUp() throws Exception {
+        //TODO Setup
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+        //TODO Tear down
+    }
+}
