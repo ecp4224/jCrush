e137b06a339b3f326753f266ca75f1eb4e99729f
Eddie <eddie@gamezgalaxy.com>
2014-03-23 17:53:48 -0400

    Added new tests

--- src/test/java/jcrush/API2Test.java
+++ src/test/java/jcrush/API2Test.java
@@ -2,12 +2,13 @@ package jcrush;
 
 import jcrush.model.Album;
 import jcrush.model.MediaCrushFile;
+import jcrush.system.Versions;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
 
 public class API2Test {
-
     @Test
     public void createAlbumTest() {
         Album album = new Album(); //Create a new local album
@@ -31,5 +32,46 @@ public class API2Test {
         }
 
         System.out.println(album.isAlbumLocal() ? "null" : album.getHash());
+    }
+
+    @Test
+    public void failedLookup() throws IOException {
+        JCrush.setAPIVersion(Versions.VERSION_1); //TODO Remove, temp code
+
+        MediaCrushFile file = JCrush.getFile("aaa");
+        if (file != null) throw new RuntimeException("File found..it wasn't suppose to exist!");
+    }
+
+    @Test
+    public void lookupFile() throws IOException {
+        JCrush.setAPIVersion(Versions.VERSION_1); //TODO Remove, temp code
+
+        MediaCrushFile m = JCrush.getFile("lS_aaVyuaAjn");
+        System.out.println("Compression: " + m.getCompression());
+        System.out.println("== FILES ==");
+        for (int i = 0; i < m.getFiles().length; i++) {
+            System.out.println((i + 1) + "). ");
+            System.out.println("  - " + m.getFiles()[i].getFile());
+            System.out.println("  - " + m.getFiles()[i].getFileType());
+        }
+        System.out.println();
+    }
+
+    //API v2 needs to be public in order for this test to work
+    public void uploadAndDelete() throws IOException {
+        System.out.println("Uploading file..");
+        File testFile = new File("src/test/test2.gif");
+        String hash = JCrush.uploadFile(testFile);
+        System.out.println("DONE: " + hash);
+        System.out.println();
+        System.out.println("Waiting 3 seconds..");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Deleting file..");
+        JCrush.delete(hash);
+        System.out.println("DONE");
     }
 }
